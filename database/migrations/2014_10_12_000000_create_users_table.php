f005822ca37a224b6f1e25392dedbdc319a7c20c
Azeem Ghafoor <azeemsulehri009@gmail.com>
2021-03-22 23:59:44 +0500

    init setup

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('parent_id')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
